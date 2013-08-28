0cefd7d80460b3537b071fd6354c00b407aaa46e
coffee-bean <mr.ziga.leber@gmail.com>
2013-08-28 23:07:49 +0200

    Tidied up member names

--- chaining_main.cpp
+++ chaining_main.cpp
@@ -1,55 +1,58 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cassert>
 using namespace std;
 
 class Container
 {
 private:
-	string str;
-	int val;
-	bool is_string;
+	string m_str;
+	int m_val;
+	bool m_is_string;
 
 public:
-	Container(string str);
-	Container(int val);
+	Container(string m_str);
+	Container(int m_val);
 
-	Container chain(int val);
+	Container chain(int m_val);
 	operator string();
 };
 
-Container::Container(string str) : str(str), is_string(true)
+Container::Container(string str) : m_str(str), m_is_string(true)
 {
 
 }
 
-Container::Container(int val) : val(val), is_string(false)
+Container::Container(int val) : m_val(val), m_is_string(false)
 {
 
 }
 
 Container Container::chain(int val)
 {
-	assert(!is_string);
-	if(this->val > val)
+	assert(!m_is_string);
+	if(m_val > val)
 	{
-		return Container("Hello");
+		stringstream ss;
+		ss<<"VAL: "<<m_val + val;
+		return Container(ss.str());
 	}
 	else
 	{
-		return Container(val+1);
+		return Container(m_val + val);
 	}
 }
 
 Container::operator string()
 {
-	assert(is_string);
-	return str;	
+	assert(m_is_string);
+	return m_str;	
 }
 	
-Container chain(int val)
+Container chain(int m_val)
 {
-	return Container(val);
+	return Container(m_val);
 }
 
 void foo(string str)
@@ -59,5 +62,5 @@ void foo(string str)
 
 int main()
 {
-	foo(chain(2).chain(1));
+	foo(chain(2).chain(5).chain(9).chain(1));
 }
